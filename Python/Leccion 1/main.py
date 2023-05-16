508f39a9b29426bbc6fe65ab61ec25b3d84cfd32
Franco Pieroni <francopiero594@gmail.com>
2023-05-16 20:34:51 -0300

    Agregando primer lección de Python terminada

---
+++ Python/Leccion 1/main.py
@@ -0,0 +1,11 @@
+miVariable = 3
+print(miVariable)
+miVariable = "Hola a todos los alumnos de la tecnicatura"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(z)
+print(id(x))
