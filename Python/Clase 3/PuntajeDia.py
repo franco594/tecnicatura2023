24a6234499c25724acf9f30180829fcc8a234f31
Franco Pieroni <francopiero594@gmail.com>
2023-05-29 20:34:16 -0300

    Agregando ejercicios PuntajeDia.py y Titulo_Autor.py clase 3

---
+++ Python/Clase 3/PuntajeDia.py
@@ -0,0 +1,7 @@
+puntajeDia = input("¿Como estuvo tu dia del 1 al 10: ")
+if int(puntajeDia) > 6:
+    print("Mi día estuvo de:", puntajeDia)
+    print(":)")
+if int(puntajeDia) < 6:
+    print("Mi día estuvo de:", puntajeDia)
+    print(":(")
