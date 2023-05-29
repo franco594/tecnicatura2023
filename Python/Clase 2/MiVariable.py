1243ddc9968b4650d44f8dad4129855c220dfbe8
Franco Pieroni <francopiero594@gmail.com>
2023-05-29 20:32:33 -0300

    Agregando Ejercicio MiVariable.py clase 2

---
+++ Python/Clase 2/MiVariable.py
@@ -0,0 +1,13 @@
+miVariable = 10
+print(miVariable)
+miVariable = "Hola a todos mis compañeros de la tecnicatura!"
+print(miVariable)
+miVariable = 3.5;
+print(miVariable)
+x = 1
+y = 2
+z = x + y
+# x = x264, y = x296, z = x328
+print(id(x))
+print(id(y))
+print(id(z))
