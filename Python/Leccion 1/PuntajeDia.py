54b3019e1c14e415b39db49b9584d4b5517d0ef9
Franco Pieroni <francopiero594@gmail.com>
2023-05-29 20:31:16 -0300

    Borrando y reubicando archivos

--- Python/Leccion 1/PuntajeDia.py
+++
@@ -1,2 +0,0 @@
-puntajeDia = input("¿Como estuvo tu dia del 1 al 10: ")
-print("Mi día estuvo de:" ,puntajeDia)
--- Python/Leccion 1/Titulo_Autor.py
+++
@@ -1,3 +0,0 @@
-titulo = input("Proporciona el titulo: ")
-autor = input("Proporciona el autor: ")
-print("" ,titulo, "fue escrito por" ,autor)
