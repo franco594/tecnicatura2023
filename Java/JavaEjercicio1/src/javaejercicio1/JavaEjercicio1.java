c3e206033f6631b81a0ba75f73dc63304cdae0e0
Franco Pieroni <francopiero594@gmail.com>
2023-05-31 19:43:45 -0300

    Agregando cambios

--- Java/JavaEjercicio1/src/javaejercicio1/JavaEjercicio1.java
+++ Java/JavaEjercicio1/src/javaejercicio1/JavaEjercicio1.java
@@ -3,16 +3,15 @@ package javaejercicio1;
 import java.util.Scanner;
 
 public class JavaEjercicio1 {
-    
+
     public static void main(String[] args) {
-     
-        
+
         Scanner entrada = new Scanner(System.in);
         System.out.println("Escriba su nombre: ");
-        var usuario2 = entrada.nextLine();
+        var usuario = entrada.nextLine();
         System.out.println("Escriba el titulo: ");
-        var titulo2 = entrada.nextLine();
-        System.out.println("Resultado: "+titulo2 +" "+ usuario2);
+        var titulo = entrada.nextLine();
+        System.out.println("Resultado: " + titulo + " " + usuario);
     }
-    
+
 }
