c44e3dc434232897ca4164e057fe3f010768531f
Franco Pieroni <francopiero594@gmail.com>
2023-06-13 00:18:39 -0300

    Agregando ejercicios clase 6

---
+++ Python/Clase6/Ejercicio 1.py
@@ -0,0 +1,6 @@
+a = float(input('ingrese el valor de a: '))
+b = float(input('ingrese el valor de b: '))
+c = float(input('ingrese el valor de c: '))
+
+resultado = ((a**3)*(b**2 - 2*a*c)) / (2*b)
+print(f'El resultado es: {resultado}')
---
+++ Python/Clase6/Ejercicio 2.py
@@ -0,0 +1,7 @@
+a = float(input('ingrese el valor de a: '))
+b = float(input('ingrese el valor de b: '))
+
+operacion = (3 + 5 * 8) < 3 and (((-6 / 3) * 4) + 2) < 2 or (a > b)
+
+print(f'El resultado es: {operacion}')
+
